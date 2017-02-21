d644d75df1d299c86d81fbb8566af865e2ede307
Alex Reyes <akosialexpo@gmail.com>
2017-02-21 15:57:18 +0000

    add moment and complete project

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
-var express = require('express')
-var app = express()
+var server = require('express')
+var app = server()
+var moment = require("moment")
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Timestamp Project!');
 });
+
+app.get('/:datestring', function(req, res) {
+    var myDate;
+    if (/^\d{8,}$/.test(req.params.datestring)) {
+        myDate = moment(req.params.datestring, "X");
+    } else {
+        myDate = moment(req.params.datestring, "MMMM D, YYYY");
+    }
+    
+    if (myDate.isValid()) {
+        res.json({
+            unix: myDate.format("X"),
+            natural: myDate.format("MMMM D, YYYY")
+        });
+    } else {
+        res.json({
+            unix: null,
+            natural: null
+        });
+    }
+})
 
 app.listen(8080, function() {
     console.log('Example app');
